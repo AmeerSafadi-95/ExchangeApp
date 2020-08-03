32b38b2c5ecee431d05190b4c83c2cfabd444cbc
AmeerSafadi-95 <ameersafadi95@gmail.com>
2020-08-03 13:30:04 +0300

    Updates

---
+++ precache-manifest.81f1a37e74fac8bc710547622ae554a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40797de521f8c266be50e94b97e2470",
+    "url": "/ExchangeApp/index.html"
+  },
+  {
+    "revision": "3f5be27cbb98cc00996d",
+    "url": "/ExchangeApp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "fe270a3f003267d3ffee",
+    "url": "/ExchangeApp/static/css/main.60f9e1d7.chunk.css"
+  },
+  {
+    "revision": "3f5be27cbb98cc00996d",
+    "url": "/ExchangeApp/static/js/2.b1f81e24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ExchangeApp/static/js/2.b1f81e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe270a3f003267d3ffee",
+    "url": "/ExchangeApp/static/js/main.f1c8000e.chunk.js"
+  },
+  {
+    "revision": "5f57a332cef49517ce1f",
+    "url": "/ExchangeApp/static/js/runtime-main.0d70a58c.js"
+  }
+]);
